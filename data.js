e560caff69f930742161a1b548247aa429070e80
YueHuawastaken <156891266+YueHuawastaken@users.noreply.github.com>
2025-11-10 09:44:29 +0000

    try commit

--- data.js
+++ data.js
@@ -1,7 +1,10 @@
-const BASE_API_URL = "https://api-open.data.gov.sg/v2/real-time/api/rainfall";
+const BASE_API_URL = "https://api-open.data.gov.sg/v1/real-time/api/rainfall";
 
 async function getWeather() {
+      
     const response = await axios.get(`${BASE_API_URL}`);
+  
     console.log(response.data);
     return response.data;
 }
+ 
